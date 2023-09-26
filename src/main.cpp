f98057e280e523ec28a27502d93f544a8117fa4a
Lucas Andreatta <drea.pierra@gmail.com>
2023-09-26 15:49:23 -0300

    implemented scikit-build-core, cmake with decent configs and working capture method from NBioAPI.

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,74 @@
+#include <iostream>
 #include <pybind11/pybind11.h>
-
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-
-int add(int i, int j) {
-    return i + j;
-}
+#include <NBioAPI.h>
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(_core, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
+NBioAPI_HANDLE nbioApiHandle;
+NBioAPI_DEVICE_ID nbioApiDeviceId;
+NBioAPI_RETURN nbioApiReturn;
+NBioAPI_VERSION version;
+NBioAPI_FIR_PURPOSE purpose;
+NBioAPI_FIR_HANDLE hCapturedFIR;
 
-        .. currentmodule:: scikit_build_example
+bool NBioBSP_Initialize() {
+    nbioApiReturn = NBioAPI_Init(&nbioApiHandle);
+    if (nbioApiReturn != NBioAPIERROR_NONE) {
+        std::cout << "NBioAPI_Init failed" << std::endl;
+        std::cout << "NBioAPI_Init Error: " << nbioApiReturn << std::endl;
+        return false;
+    }
+    std::cout << "NBioAPI_Init Success (Handle Value): " << nbioApiHandle << std::endl;
+    return true;
+}
 
-        .. autosummary::
-           :toctree: _generate
+void NBioBSP_GetVersion() {
+    nbioApiReturn = NBioAPI_GetVersion(nbioApiHandle, &version);
+    if (nbioApiReturn != NBioAPIERROR_NONE) {
+        std::cout << "NBioAPI_GetVersion failed" << std::endl;
+        std::cout << "NBioAPI_GetVersion Error: " << nbioApiReturn << std::endl;
+        return;
+    }
+    std::cout << "NBioAPI_GetVersion: " << version.Major << "." << version.Minor << "." <<  std::endl;
+}
 
-           add
-           subtract
-    )pbdoc";
+void NBioBSP_OpenDevice(){
+    nbioApiDeviceId = NBioAPI_DEVICE_ID_AUTO;
+    nbioApiReturn = NBioAPI_OpenDevice(nbioApiHandle, nbioApiDeviceId);
+    if (nbioApiReturn != NBioAPIERROR_NONE) {
+        std::cout << "NBioAPI_OpenDevice failed" << std::endl;
+        std::cout << "NBioAPI_OpenDevice Error: " << nbioApiReturn << std::endl;
+        return;
+    }
+    std::cout << "NBioAPI_OpenDevice Success (Device ID): " << nbioApiDeviceId << std::endl;
+}
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+void NBioBSP_CloseDevice(){
+    nbioApiReturn = NBioAPI_CloseDevice(nbioApiHandle, nbioApiDeviceId);
+    if (nbioApiReturn != NBioAPIERROR_NONE) {
+        std::cout << "NBioAPI_CloseDevice failed" << std::endl;
+        std::cout << "NBioAPI_CloseDevice Error: " << nbioApiReturn << std::endl;
+        return;
+    }
+    std::cout << "NBioAPI_CloseDevice Success" << std::endl;
+}
 
-        Some other explanation about the add function.
-    )pbdoc");
+void NBioBSP_Capture(){
+    purpose = NBioAPI_FIR_PURPOSE_VERIFY;
+    nbioApiReturn = NBioAPI_Capture(nbioApiHandle, purpose, &hCapturedFIR, 10000, NULL, NULL);
+    if (nbioApiReturn != NBioAPIERROR_NONE) {
+        std::cout << "NBioAPI_Capture failed" << std::endl;
+        std::cout << "NBioAPI_Capture Error: " << nbioApiReturn << std::endl;
+        return;
+    }
+    std::cout << "NBioAPI_Capture Success" << std::endl;
+}
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+PYBIND11_MODULE(_core, module) {
+    module.doc() = "Python binding for NBioBSP";
+    module.def("initialize", &NBioBSP_Initialize, "Initialize NBioBSP");
+    module.def("get_version", &NBioBSP_GetVersion, "Get NBioBSP Version");
+    module.def("open_device", &NBioBSP_OpenDevice, "Open NBioBSP Device");
+    module.def("close_device", &NBioBSP_CloseDevice, "Close NBioBSP Device");
+    module.def("capture", &NBioBSP_Capture, "Capture NBioBSP Device");
 }
---
+++ src/nbsp_python/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from ._core import initialize, get_version, open_device, close_device, capture
+
+__all__ = ["initialize", "get_version", "open_device", "close_device", "capture"]
