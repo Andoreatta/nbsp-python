000be923ee0dac7aaa9ef59f9537940435542980
Lucas Andreatta <drea.pierra@gmail.com>
2023-09-29 13:34:59 -0300

    Added custom exception handler

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,14 @@
 // so dont judge me
 // C++11
 #include <iostream>
+#include <unordered_map>
+#include <exception>
+
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-
 #include <NBioAPI.h>
-#include <unordered_map>
 
 namespace py = pybind11;
-
-// The ERRORs are the sum of hex values from NBioAPI_ERROR.h
-// yet, it will dump you an decimal value, so you have to convert it to hex
-// e.g:  NBioAPIERROR_BASE_DEVICE          (0x0100)
-//       NBioAPIERROR_DEVICE_OPEN_FAIL     (NBioAPIERROR_BASE_DEVICE + 0x01) returns 257
-// 257 in hex is 0x101
 
 // vars that are reused constantly
 // depending on the function, other types are declared inside the function scope
@@ -26,6 +21,203 @@ NBioAPI_FIR_PURPOSE purpose;
 NBioAPI_FIR_HANDLE hCapturedFIR;
 NBioAPI_FIR_TEXTENCODE textFIR;
 
+// custom exception handler class
+class NBioAPIException : public std::exception {
+public:
+    NBioAPIException(NBioAPI_RETURN errCode) : errorCode(errCode) {}
+    virtual const char* what() const throw() {
+        switch (errorCode) {
+        case NBioAPIERROR_INVALID_HANDLE:
+            return "Invalid handle";
+        case NBioAPIERROR_INVALID_POINTER:
+            return "Invalid pointer";
+        case NBioAPIERROR_INVALID_TYPE:
+            return "Invalid type";
+        case NBioAPIERROR_FUNCTION_FAIL:
+            return "Function failed";
+        case NBioAPIERROR_STRUCTTYPE_NOT_MATCHED:
+            return "Structure type not matched";
+        case NBioAPIERROR_ALREADY_PROCESSED:
+            return "Data already processed";
+        case NBioAPIERROR_EXTRACTION_OPEN_FAIL:
+            return "Extraction open failed";
+        case NBioAPIERROR_VERIFICATION_OPEN_FAIL:
+            return "Verification open failed";
+        case NBioAPIERROR_DATA_PROCESS_FAIL:
+            return "Data processing failed";
+        case NBioAPIERROR_MUST_BE_PROCESSED_DATA:
+            return "Data must be processed";
+        case NBioAPIERROR_INTERNAL_CHECKSUM_FAIL:
+            return "Internal checksum failed";
+        case NBioAPIERROR_ENCRYPTED_DATA_ERROR:
+            return "Encrypted data error";
+        case NBioAPIERROR_UNKNOWN_FORMAT:
+            return "Unknown format";
+        case NBioAPIERROR_UNKNOWN_VERSION:
+            return "Unknown version";
+        case NBioAPIERROR_VALIDITY_FAIL:
+            return "Validity check failed";
+        case NBioAPIERROR_INIT_MAXFINGER:
+            return "Max finger initialization failed";
+        case NBioAPIERROR_INIT_SAMPLESPERFINGER:
+            return "Samples per finger initialization failed";
+        case NBioAPIERROR_INIT_ENROLLQUALITY:
+            return "Enroll quality initialization failed";
+        case NBioAPIERROR_INIT_VERIFYQUALITY:
+            return "Verify quality initialization failed";
+        case NBioAPIERROR_INIT_IDENTIFYQUALITY:
+            return "Identify quality initialization failed";
+        case NBioAPIERROR_INIT_SECURITYLEVEL:
+            return "Security level initialization failed";
+        case NBioAPIERROR_INVALID_MINSIZE:
+            return "Invalid min size";
+        case NBioAPIERROR_INVALID_TEMPLATE:
+            return "Invalid template";
+        case NBioAPIERROR_EXPIRED_VERSION:
+            return "Expired version";
+        case NBioAPIERROR_INVALID_SAMPLESPERFINGER:
+            return "Invalid samples per finger";
+        case NBioAPIERROR_UNKNOWN_INPUTFORMAT:
+            return "Unknown input format";
+        case NBioAPIERROR_INIT_ENROLLSECURITYLEVEL:
+            return "Enroll security level initialization failed";
+        case NBioAPIERROR_INIT_NECESSARYENROLLNUM:
+            return "Necessary enroll number initialization failed";
+        case NBioAPIERROR_INIT_RESERVED1:
+            return "Reserved initialization error 1";
+        case NBioAPIERROR_INIT_RESERVED2:
+            return "Reserved initialization error 2";
+        case NBioAPIERROR_INIT_RESERVED3:
+            return "Reserved initialization error 3";
+        case NBioAPIERROR_INIT_RESERVED4:
+            return "Reserved initialization error 4";
+        case NBioAPIERROR_INIT_RESERVED5:
+            return "Reserved initialization error 5";
+        case NBioAPIERROR_INIT_RESERVED6:
+            return "Reserved initialization error 6";
+        case NBioAPIERROR_INIT_RESERVED7:
+            return "Reserved initialization error 7";
+        case NBioAPIERROR_OUT_OF_MEMORY:
+            return "Out of memory";
+        case NBioAPIERROR_DEVICE_OPEN_FAIL:
+            return "Device open failed";
+        case NBioAPIERROR_INVALID_DEVICE_ID:
+            return "Invalid device ID";
+        case NBioAPIERROR_WRONG_DEVICE_ID:
+            return "Wrong device ID";
+        case NBioAPIERROR_DEVICE_ALREADY_OPENED:
+            return "Device already opened";
+        case NBioAPIERROR_DEVICE_NOT_OPENED:
+            return "Device not opened";
+        case NBioAPIERROR_DEVICE_BRIGHTNESS:
+            return "Device brightness error";
+        case NBioAPIERROR_DEVICE_CONTRAST:
+            return "Device contrast error";
+        case NBioAPIERROR_DEVICE_GAIN:
+            return "Device gain error";
+        case NBioAPIERROR_LOWVERSION_DRIVER:
+            return "Low version driver error";
+        case NBioAPIERROR_DEVICE_INIT_FAIL:
+            return "Device initialization failed";
+        case NBioAPIERROR_DEVICE_LOST_DEVICE:
+            return "Device lost";
+        case NBioAPIERROR_DEVICE_DLL_LOAD_FAIL:
+            return "Device DLL load failed";
+        case NBioAPIERROR_DEVICE_MAKE_INSTANCE_FAIL:
+            return "Device instance creation failed";
+        case NBioAPIERROR_DEVICE_DLL_GET_PROC_FAIL:
+            return "Device DLL get procedure failed";
+        case NBioAPIERROR_DEVICE_IO_CONTROL_FAIL:
+            return "Device IO control failed";
+        case NBioAPIERROR_DEVICE_NOT_SUPPORT:
+            return "Device not supported";
+        case NBioAPIERROR_DEVICE_LFD:
+            return "Device LFD error";
+        case NBioAPIERROR_INVALID_DEVICE_CODE:
+            return "Invalid device code";
+        case NBioAPIERROR_DEVICE_NOT_FOUND:
+            return "Device not found";
+        case NBioAPIERROR_USER_CANCEL:
+            return "User cancelled";
+        case NBioAPIERROR_USER_BACK:
+            return "User went back";
+        case NBioAPIERROR_CAPTURE_TIMEOUT:
+            return "Capture timeout";
+        case NBioAPIERROR_CAPTURE_FAKE_SUSPICIOUS:
+            return "Fake finger detected";
+        case NBioAPIERROR_ENROLL_EVENT_PLACE:
+            return "Place finger on sensor";
+        case NBioAPIERROR_ENROLL_EVENT_HOLD:
+            return "Hold finger on sensor";
+        case NBioAPIERROR_ENROLL_EVENT_REMOVE:
+            return "Remove finger from sensor";
+        case NBioAPIERROR_ENROLL_EVENT_PLACE_AGAIN:
+            return "Place finger on sensor again";
+        case NBioAPIERROR_ENROLL_EVENT_EXTRACT:
+            return "Extracting template";
+        case NBioAPIERROR_ENROLL_EVENT_MATCH_FAILED:
+            return "Enrollment match failed";
+        case NBioAPIERROR_INIT_MAXCANDIDATE:
+            return "Max candidate initialization failed";
+        case NBioAPIERROR_NSEARCH_OPEN_FAIL:
+            return "NSearch open failed";
+        case NBioAPIERROR_NSEARCH_INIT_FAIL:
+            return "NSearch initialization failed";
+        case NBioAPIERROR_NSEARCH_MEM_OVERFLOW:
+            return "NSearch memory overflow";
+        case NBioAPIERROR_NSEARCH_SAVE_DB:
+            return "NSearch save database failed";
+        case NBioAPIERROR_NSEARCH_LOAD_DB:
+            return "NSearch load database failed";
+        case NBioAPIERROR_NSEARCH_INVALD_TEMPLATE:
+            return "NSearch invalid template";
+        case NBioAPIERROR_NSEARCH_OVER_LIMIT:
+            return "NSearch over limit";
+        case NBioAPIERROR_NSEARCH_IDENTIFY_FAIL:
+            return "NSearch identification failed";
+        case NBioAPIERROR_NSEARCH_LICENSE_LOAD:
+            return "NSearch license load failed";
+        case NBioAPIERROR_NSEARCH_LICENSE_KEY:
+            return "NSearch license key error";
+        case NBioAPIERROR_NSEARCH_LICENSE_EXPIRED:
+            return "NSearch license expired";
+        case NBioAPIERROR_NSEARCH_DUPLICATED_ID:
+            return "NSearch duplicated ID";
+        case NBioAPIERROR_NSEARCH_INVALID_ID:
+            return "NSearch invalid ID";
+        case NBioAPIERROR_IMGCONV_INVALID_PARAM:
+            return "Image conversion invalid parameter";
+        case NBioAPIERROR_IMGCONV_MEMALLOC_FAIL:
+            return "Image conversion memory allocation failed";
+        case NBioAPIERROR_IMGCONV_FILEOPEN_FAIL:
+            return "Image conversion file open failed";
+        case NBioAPIERROR_IMGCONV_FILEWRITE_FAIL:
+            return "Image conversion file write failed";
+        case NBioAPIERROR_INIT_PRESEARCHRATE:
+            return "Presearch rate initialization failed";
+        case NBioAPIERROR_INDEXSEARCH_INIT_FAIL:
+            return "Index search initialization failed";
+        case NBioAPIERROR_INDEXSEARCH_SAVE_DB:
+            return "Index search save database failed";
+        case NBioAPIERROR_INDEXSEARCH_LOAD_DB:
+            return "Index search load database failed";
+        case NBioAPIERROR_INDEXSEARCH_UNKNOWN_VER:
+            return "Index search unknown version";
+        case NBioAPIERROR_INDEXSEARCH_IDENTIFY_FAIL:
+            return "Index search identification failed";
+        case NBioAPIERROR_INDEXSEARCH_DUPLICATED_ID:
+            return "Index search duplicated ID";
+        case NBioAPIERROR_INDEXSEARCH_IDENTIFY_STOP:
+            return "Index search identification stopped";
+        default:
+            return "Unknown error";
+        }
+    }
+private:
+    NBioAPI_RETURN errorCode;
+};
+
+// purpose map
 std::unordered_map<std::string, NBioAPI_FIR_PURPOSE> purposeMap = {
     {"verify", NBioAPI_FIR_PURPOSE_VERIFY},
     {"identify", NBioAPI_FIR_PURPOSE_IDENTIFY},
